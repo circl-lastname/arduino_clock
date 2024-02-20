78efbd2aa9f8b2e862c1eab55450a5d9ad123041
circl <circl.lastname@gmail.com>
2024-02-20 21:58:41 +0100

    Initial commit

---
+++ pins.h
@@ -0,0 +1,15 @@
+#define BTN_1 2
+#define BTN_2 4
+
+#define SPKR 3
+
+#define BKLT 6
+
+#define LCD_RS 7
+#define LCD_E 8
+#define LCD_B4 9
+#define LCD_B5 10
+#define LCD_B6 11
+#define LCD_B7 12
+
+#define LED 13
